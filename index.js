931c2e0fc402457c8b5763c89e292b5a42fc9659
unknown <nabilrobertdeniro@gmail.com>
2025-06-07 14:40:55 +0700

    backend and ejs functional

---
+++ index.js
@@ -0,0 +1,32 @@
+import express from "express";
+import axios from "axios";
+
+const app = express();
+const port = 3000;
+
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.render("index.ejs", { 
+    quote: null,
+    author: null
+   });
+});
+app.post("/get-quote", async (req,res) => {
+    try {
+        const result = await axios.get("https://aot-api.vercel.app/quote");
+        res.render("index.ejs", 
+        {
+            quote: result.data.quote.quote,
+            author: result.data.quote.character,
+        });
+} catch (error) {
+    res.render("index", {
+      quote: null,
+      author: null
+    })
+};
+});
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
